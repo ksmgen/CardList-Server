5e479ee2a99fe1bc254ba6ee17490096e1f0ab61
Claudia <88203931+clauculus@users.noreply.github.com>
2022-10-25 19:13:24 +0700

    update cardlist query

--- controllers/cardlist.controller.js
+++ controllers/cardlist.controller.js
@@ -4,7 +4,7 @@ exports.cardlist = async (req, res) => {
   const results = await db
     .promise()
     .query(
-      `SELECT * FROM card, category WHERE card.category_id = category.category_id`
+      `SELECT card.SKU AS SKU, category.name AS name, category.category_desc AS category_desc, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id`
     );
   res.json(results[0]);
 };
