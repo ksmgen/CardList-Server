69a29e4f689baae2bb4ff5f9c6983e0ce7ecf40e
Claudia <88203931+clauculus@users.noreply.github.com>
2022-11-01 13:22:57 +0700

    add test api for rn

--- controllers/cardlist.controller.js
+++ controllers/cardlist.controller.js
@@ -1,10 +1,22 @@
 const db = require("../database");
 
-exports.cardlist = async (req, res) => {
+exports.card_list = async (req, res) => {
   const results = await db
     .promise()
     .query(
       `SELECT card.SKU AS SKU, category.name AS name, category.category_desc AS category_desc, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id`
     );
   res.json(results[0]);
+};
+
+exports.card_detail = async (req, res) => {
+  const card_id = req.params.id;
+  const sql =
+    "SELECT card.SKU AS SKU, category.name AS name, category.category_desc AS category_desc, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id AND card.id = ?";
+  const results = await db.promise().query(sql, card_id);
+  res.json(results[0]);
+};
+
+exports.card_test = async (req, res) => {
+  res.json({ name: "Card 1" });
 };
--- routes/cardlist.routes.js
+++ routes/cardlist.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const card_controller = require("../controllers/cardlist.controller");
 
-router.get("/", card_controller.cardlist);
+router.get("/", card_controller.card_list);
+router.get("/detail/:id", card_controller.card_detail);
+router.get("/test", card_controller.card_test);
 
 module.exports = router;
