785725891bbe809ddc771917aaa8b6c75f53ad31
putriliza <putriliza05@gmail.com>
2022-11-10 18:04:37 +0700

    API for addCard, fixing cardlist query

--- controllers/cardlist.controller.js
+++ controllers/cardlist.controller.js
@@ -4,7 +4,7 @@ exports.card_list = async (req, res) => {
   const results = await db
     .promise()
     .query(
-      `SELECT card.SKU AS SKU, category.name AS name, category.description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id`
+      `SELECT card.SKU AS SKU, category.name AS name, category.description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card LEFT JOIN category USING(category_id)`
     );
   res.json(results[0]);
 };
@@ -16,6 +16,38 @@ exports.card_detail = async (req, res) => {
   const results = await db.promise().query(sql, card_id);
   res.json(results[0]);
 };
+
+exports.add_card = async (req, res) => {
+  const card = req.body;
+  const sql =
+    "INSERT INTO card (SKU, name, description, category_id, image, grade, nation, rarity, race, critical, illustrator, power, regulation, shield, skill, trigger_type, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  const results = await db
+    .promise()
+    .query(sql, [
+      card.SKU,
+      card.name,
+      card.description,
+      card.category_id,
+      card.image,
+      parseInt(card.grade),
+      card.nation,
+      card.rarity,
+      card.race,
+      parseInt(card.critical),
+      card.illustrator,
+      parseInt(card.power),
+      card.regulation,
+      parseInt(card.shield),
+      card.skill,
+      card.trigger_type,
+      card.type,
+    ]);
+  
+  res.json(results[0]);
+  console.log(card);
+};
+
+
 
 exports.card_test = async (req, res) => {
   res.json({ name: "Card 1" });
--- routes/cardlist.routes.js
+++ routes/cardlist.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const card_controller = require("../controllers/cardlist.controller");
+const bp = require('body-parser')
+
+router.use(bp.json())
+router.use(bp.urlencoded({ extended: true }))
 
 router.get("/", card_controller.card_list);
 router.get("/detail/:id", card_controller.card_detail);
+router.post("/addCard", card_controller.add_card);
 router.get("/test", card_controller.card_test);
 
 module.exports = router;
