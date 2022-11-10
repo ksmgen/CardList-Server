2af2338dcd84b95e1636dde7dd187a5c8a3d5fa9
putriliza <putriliza05@gmail.com>
2022-11-10 13:59:51 +0700

    resetup database & api for get cardlist

--- controllers/cardlist.controller.js
+++ controllers/cardlist.controller.js
@@ -4,7 +4,7 @@ exports.card_list = async (req, res) => {
   const results = await db
     .promise()
     .query(
-      `SELECT card.SKU AS SKU, category.name AS name, category.category_desc AS category_desc, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id`
+      `SELECT card.SKU AS SKU, category.name AS name, category.description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id`
     );
   res.json(results[0]);
 };
@@ -12,7 +12,7 @@ exports.card_list = async (req, res) => {
 exports.card_detail = async (req, res) => {
   const card_id = req.params.id;
   const sql =
-    "SELECT card.SKU AS SKU, category.name AS name, category.category_desc AS category_desc, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id AND card.id = ?";
+    "SELECT card.SKU AS SKU, category.name AS name, category.description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id AND card.id = ?";
   const results = await db.promise().query(sql, card_id);
   res.json(results[0]);
 };
