104d31e5f61b67d45a8291523f922bdf45ae3586
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-11-17 16:20:00 +0700

    connect to remote db

--- controllers/cardlist.controller.js
+++ controllers/cardlist.controller.js
@@ -4,7 +4,7 @@ exports.card_list = async (req, res) => {
   const results = await db
     .promise()
     .query(
-      `SELECT card.SKU AS SKU, category.name AS name, category.description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card LEFT JOIN category USING(category_id)`
+      `SELECT card.SKU AS SKU, category.name AS name, category.category_description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card LEFT JOIN category USING(category_id)`
     );
   res.json(results[0]);
 };
@@ -12,7 +12,7 @@ exports.card_list = async (req, res) => {
 exports.card_detail = async (req, res) => {
   const card_id = req.params.id;
   const sql =
-    "SELECT card.SKU AS SKU, category.name AS name, category.description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id AND card.id = ?";
+    "SELECT card.SKU AS SKU, category.name AS name, category.category_description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card, category WHERE card.category_id = category.category_id AND card.id = ?";
   const results = await db.promise().query(sql, card_id);
   res.json(results[0]);
 };
@@ -50,7 +50,7 @@ exports.add_card = async (req, res) => {
 exports.add_category = async (req, res) => {
   const category = req.body;
   const sql =
-    "INSERT INTO category (name, description) VALUES (?, ?)";
+    "INSERT INTO category (name, category_description) VALUES (?, ?)";
   const results = await db
     .promise()
     .query(sql, [
--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 
 module.exports = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
+  host: "kualalumpur-2.ceysltqt5ig6.ap-southeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "OldKlangRoad#46000",
   database: "cardlist",
 });
