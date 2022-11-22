b3985ef7ffe3ab18aa2601b6df1353867bafb412
putriliza <putriliza05@gmail.com>
2022-11-22 15:29:24 +0700

    change cardlist detail endpoint

--- controllers/cardlist.controller.js
+++ controllers/cardlist.controller.js
@@ -4,7 +4,7 @@ exports.card_list = async (req, res) => {
   const results = await db
     .promise()
     .query(
-      `SELECT card.SKU AS SKU, category.name AS name, category.category_description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card LEFT JOIN category USING(category_id)`
+      `SELECT card.SKU AS SKU, card.name AS name, category.name as category, category.category_description AS category_description, card.image AS image, card.grade AS grade, card.nation AS nation, card.rarity AS rarity, card.race AS race, card.critical AS critical, card.illustrator AS illustrator, card.power AS power, card.regulation AS regulation, card.shield AS shield, card.skill AS skill, card.trigger_type AS trigger_type, card.type AS type, CONVERT (card.description USING utf8) AS description FROM card LEFT JOIN category USING(category_id)`
     );
   res.json(results[0]);
 };
