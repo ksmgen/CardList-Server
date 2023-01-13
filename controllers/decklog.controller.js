9a7fc4ff2b1333fec56cfd6e204046d803aab0fd
putriliza <putriliza05@gmail.com>
2023-01-13 17:23:34 +0700

    add decklog controller

--- controllers/decklog.controller.js
+++ controllers/decklog.controller.js
@@ -10,3 +10,82 @@ exports.decklog = async (req, res) => {
     console.log(error);
   }
 };
+
+exports.get_all_cards = async (req, res) => {
+  try {
+    const results = await db.promise().query(`SELECT * FROM oracle_cards_dev`);
+    res.json(results[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.search = async (req, res) => {
+  // ?nation=&keyword=&cardTypes=&grades=&minPower=&maxPower=&rarity=&triggers=
+  console.log(req.query);
+  const nation = req.query.nation;
+  const keyword = req.query.keyword;
+  const cardTypes = req.query.cardTypes;
+  const cardTypesArr = cardTypes.split(",").map((e) => `'${e}'`);
+  const grades = req.query.grades;
+  const gradesArr = grades.split(",");
+  const minPower = req.query.minPower;
+  const maxPower = req.query.maxPower;
+  const rarity = req.query.rarity;
+  const triggers = req.query.triggers;
+  const triggersArr = triggers.split(",").map((e) => `'${e}'`);
+
+  const table = "oracle_cards_vairina";
+
+  try {
+    let sql = ` SELECT  *, CONVERT (text USING utf8) as text2
+                FROM    ${table} 
+                WHERE   (published = 1) `;
+    
+    if (nation) {
+      sql += `AND (nation = '${nation}') `;
+    }
+
+    if (keyword) {
+      sql += `AND (name LIKE '%${keyword}%') `;
+    }
+
+    if (cardTypes) {
+      sql += `AND (type IN (${cardTypesArr})) `;
+    }
+
+    if (grades) {
+      sql += `AND (grade IN (${gradesArr})) `;
+    }
+
+    if (minPower) {
+      sql += `AND (power >= ${minPower}) `;
+    }
+
+    if (maxPower) {
+      sql += `AND (power <= ${maxPower}) `;
+    }
+
+    if (rarity) {
+      sql += `AND (rarity = '${rarity}') `;
+    }
+
+    if (triggers) {
+      sql += `AND ( FALSE `;
+      triggersArr.forEach(val => {
+        sql += `OR trigger_text LIKE '%${val}%' `;
+        });
+      sql += `) `;
+    }
+
+    sql += `ORDER BY SKU`;
+  
+    console.log(sql);
+
+    const results = await db.promise().query(sql);
+    res.json(results[0]);
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/decklog.routes.js
+++ routes/decklog.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 const router = Router();
 
-import { decklog } from "../controllers/decklog.controller";
+import {
+decklog,
+get_all_cards,
+search,
+} from "../controllers/decklog.controller";
+
 import { json, urlencoded } from "body-parser";
 
 router.use(json());
 router.use(urlencoded({ extended: true }));
 
 router.get("/", decklog);
+router.get("/get_all_cards", get_all_cards);
+router.get("/search", search);
 
 export default router;
